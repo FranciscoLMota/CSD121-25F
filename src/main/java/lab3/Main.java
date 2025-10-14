cb2d4b2fbd1ed23896630c2fd93d6bf064ddada2
Francisco Mota <franciscolmota@outlook.com>
2025-10-14 10:31:15 -0400

    Added the base of the board and plays

--- src/main/java/lab3/Main.java
+++ src/main/java/lab3/Main.java
@@ -1,18 +1,25 @@
-package lab3;
+package main.java.lab3;
+
+import main.java.lab3.game.Board;
+import main.java.lab3.game.Play;
 
 public class Main {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
 
-        // TODO: Initialize a TicTacToe game with a 3x3 board and two players
+        // Initialize a TicTacToe game with a 3x3 board and two players
+//        var game = new Board();
+//
+//        game.setPosition(0,0, Play.EMPTY);
+//
+//        IO.print(game.getPosition(0, 0).toString());
 
         while (true) {
             // TODO: Display the board
 
             // TODO: Check if the game is over or a draw, and if so display the result and exit
-            
-            // TODO: Get the next move from the player and update the game state
 
+            // TODO: Get the next move from the player and update the game state
         }
     }
 }
--- src/main/java/lab3/game/Board.java
+++ src/main/java/lab3/game/Board.java
@@ -1,5 +1,22 @@
-package lab3.game;
+package main.java.lab3.game;
+
+import static main.java.lab3.game.Play.*;
 
 public class Board {
     // TODO: encapsulate the representation of the tictactoe board and provide instance methods to access and update it
+
+    public Play[][] board = {
+            {EMPTY, EMPTY, EMPTY},
+            {EMPTY, EMPTY, EMPTY},
+            {EMPTY, EMPTY, EMPTY}
+    };
+
+    public Play getPosition(int row, int col) {
+        return board[row][col];
+    }
+
+    public void setPosition(int row, int col, Play mark) {
+        board[row][col] = mark;
+    }
+
 }
