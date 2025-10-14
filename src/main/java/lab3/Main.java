ff8447f00759356db327e38eb33e0a7b4a5430ba
Francisco Mota <franciscolmota@outlook.com>
2025-10-14 12:02:25 -0400

    check wincondition

--- src/main/java/lab3/Main.java
+++ src/main/java/lab3/Main.java
@@ -8,18 +8,16 @@ public class Main {
     static void main(String[] args) {
 
         // Initialize a TicTacToe game with a 3x3 board and two players
-//        var game = new Board();
+        var game = new Board();
+
+        IO.print(game.checkWin(Play.PO));
+
+//        while (true) {
+//            // TODO: Display the board
 //
-//        game.setPosition(0,0, Play.EMPTY);
+//            // TODO: Check if the game is over or a draw, and if so display the result and exit
 //
-//        IO.print(game.getPosition(0, 0).toString());
-
-        while (true) {
-            // TODO: Display the board
-
-            // TODO: Check if the game is over or a draw, and if so display the result and exit
-
-            // TODO: Get the next move from the player and update the game state
-        }
+//            // TODO: Get the next move from the player and update the game state
+//        }
     }
 }
--- src/main/java/lab3/game/Board.java
+++ src/main/java/lab3/game/Board.java
@@ -8,7 +8,7 @@ public class Board {
     public Play[][] board = {
             {EMPTY, EMPTY, EMPTY},
             {EMPTY, EMPTY, EMPTY},
-            {EMPTY, EMPTY, EMPTY}
+            {EMPTY, EMPTY, EMPTY},
     };
 
     public Play getPosition(int row, int col) {
@@ -17,6 +17,39 @@ public class Board {
 
     public void setPosition(int row, int col, Play mark) {
         board[row][col] = mark;
+    }
+
+    /**
+     * Checks if there has been a win on the board, should be called after each play.
+     *
+     */
+    public boolean checkWin(Play play) {
+
+        //Checks for row/column wins
+        for (int row = 0; row < board.length; row++) {
+            boolean rowWin = true;
+            boolean colWin = true;
+
+            for (int col = 0; col < board[row].length; col++) {
+                if (board[row][col] != play) rowWin = false;
+                if (board[col][row] != play) colWin = false;
+            }
+
+            if (rowWin || colWin) {
+                return true;
+            }
+
+        }
+
+        //Checks for diagonal wins
+        //TODO: improve this somehow
+        if ((board[0][0] == play && board[1][1] == play && board[2][2] == play) ||
+                board[0][2] == play && board[1][1] == play && board[2][0] == play
+        ) {
+            return true;
+        }
+
+        return false;
     }
 
 }
